49e5b79e1f1926ae21fad378e8994d9f68ddbfce
abdo-elmorsi <abdelrahmandiv@gmail.com>
2024-06-28 19:00:56 +0300

    update rn app link

--- app/assets/user-data.js
+++ app/assets/user-data.js
@@ -9,7 +9,7 @@ export const userData = {
     stackOverflow: "https://stackoverflow.com/users/17125894/abdo-elmorsy",
     leetcode: "https://leetcode.com/abdo-elmorsi/",
     resume: "https://drive.google.com/file/d/1kfM4Bvt8fO3L68BRt-H_pFgh-v9SNfk6/view?usp=sharing",
-    rnVersion: "https://drive.google.com/file/d/1Am3G9L3OesCTYKv91QDCEWzuOO6S5WZw/view?usp=sharing",
+    rnVersion: "https://drive.google.com/file/d/1G7BPTwcdvwA6P3urovZFW_wxqWb2S18u/view?usp=sharing",
     skills: [
         'React',
         'Next.js',
