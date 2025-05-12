f51119cf4dc2e4d151277e68cd9f18ca6515cac0
abdo-elmorsi <abdelrahmandiv@gmail.com>
2025-05-13 01:48:14 +0300

    google-site-verification

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { GoogleTagManager } from "@next/third-parties/google";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 import { Inter } from "next/font/google";
 import Footer from "./components/footer";
@@ -10,6 +10,12 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <meta
+                    name="google-site-verification"
+                    content="x3lb8KGwipadQnoT80mEZi86HD_vCO2VMWUc4LR6Nw8"
+                />
+            </head>
             <body className={inter.className}>
                 <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
                     {children}
