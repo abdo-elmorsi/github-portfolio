2d31b6380b5222dac04cf32fd6a84c23359ee2d7
abdo-elmorsi <abdelrahmandiv@gmail.com>
2025-08-25 14:29:35 +0300

    seo improvements

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -8,6 +8,8 @@ export async function GET() {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>${baseUrl}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
     <priority>1.0</priority>
   </url>
 </urlset>`;
